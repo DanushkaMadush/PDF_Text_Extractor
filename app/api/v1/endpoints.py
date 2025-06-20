e344176bd3232fe4fa508e34400ab650caebb5a8
danushkadi <danushkad@ccl.lk>
2025-06-20 11:13:24 +0530

    feat(endpoints.py):Added pdf extracting process

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -2,13 +2,24 @@
 
 from fastapi import APIRouter, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
+import logging
+from app.services.extractor import process_pdf
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 @router.post("/extract")
 async def extract_pdf(file: UploadFile = File(...)):
     if not file.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="Only PDF files are supported.")
+    
+    try:
+        result = process_pdf(file)
+        return JSONResponse(content=result)
+    
+    except Exception as e:
+        logger.error(f"Failed to response PDF : {e}")
+        raise HTTPException(status_code=500 , detail="Internal PDF processing PDF error")
 
     # Placeholder logic until we implement extractor service
     return JSONResponse({
