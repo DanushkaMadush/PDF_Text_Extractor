9e89eecacfc75a786f4d0bc457d814463ad1de9d
danushkadi <danushkad@ccl.lk>
2025-06-20 11:27:30 +0530

    perf(endpoints.py):Added extract response interface to the endpoint

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -4,11 +4,12 @@ from fastapi import APIRouter, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 import logging
 from app.services.extractor import process_pdf
+from app.models.extract_response import ExtractResponse
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
-@router.post("/extract")
+@router.post("/extract" , response_model=ExtractResponse)
 async def extract_pdf(file: UploadFile = File(...)):
     if not file.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="Only PDF files are supported.")
